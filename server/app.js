6eb6d94153dd29898bc58fce94a73b0ffce85ac8
Oriana <oriana.nohemi22@gmail.com>
2021-05-16 18:13:33 -0500

    servidor

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require("cors");
+const config = require("./config");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+router(app)
+
+app.listen(config.port, () => {
+  console.log(`Server app listening at ${config.host}:${config.port}`)
+})
---
+++ server/config.js
@@ -0,0 +1,7 @@
+const config = {
+    port: process.env.PORT || 3200,
+    host: process.env.HOST || "http://localhost",
+    apiUrl: 'https://api.mercadolibre.com/sites/MLA/',
+  };
+  
+  module.exports = config;
