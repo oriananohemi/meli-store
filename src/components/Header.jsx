1ac9d7b505092cd2d02294e71bece99bcba0d77d
Oriana <oriana.nohemi22@gmail.com>
2021-05-16 14:49:10 -0500

    tomar el valor del input y redirigir a otro componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import brand from '../assets/images/brand.png';
 import search from '../assets/images/search.png';
 
 const Header = () => {
+    const history = useHistory()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let input = e.target["query"];
+        const url = `/items?seach=${input.value}`;
+        input.value = ''
+        history.push(url);
+    }
+
     return (
         <header className="header">
-            <img src={brand} alt="Meli logo" />
-            <div className="center-container header__search-container">
-                <input className="header__input" type="text" placeholder="Nunca dejes de buscar" />
-                <div className="header__search">
-                    <img src={search} alt="Buscador" />
-                </div>
-            </div>
+            <Link to="/">
+                <img src={brand} alt="Meli logo" />
+            </Link>
+                <form className="center-container header__search-container" onSubmit={handleSubmit}>
+                    <input className="header__input" name="query" type="text" placeholder="Nunca dejes de buscar" />
+                    <button className="header__search">
+                        <img src={search} alt="Buscador" />
+                    </button>
+                </form>
         </header>
     )
 }
