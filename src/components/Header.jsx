b7ae3401cd4f2e866c0299c4aeb10ee732de47a2
Oriana <oriana.nohemi22@gmail.com>
2021-05-15 21:53:26 -0500

    estructura y estilos del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import brand from '../assets/images/brand.png'
+import brand from '../assets/images/brand.png';
+import search from '../assets/images/search.png';
 
 const Header = () => {
     return (
-        <header>
+        <header className="header">
             <img src={brand} alt="Meli logo" />
+            <div className="center-container header__search-container">
+                <input className="header__input" type="text" placeholder="Nunca dejes de buscar" />
+                <div className="header__search">
+                    <img src={search} alt="Buscador" />
+                </div>
+            </div>
         </header>
     )
 }
