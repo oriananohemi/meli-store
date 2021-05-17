2496d4affd12da216090e3493f9fcc7aa2163b84
Oriana <oriana.nohemi22@gmail.com>
2021-05-17 10:06:50 -0500

    mostrar resultados de la busqueda

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Items from '../components/Items';
+import { getArticlesList } from '../services/requestHandler';
 
 const Results = () => {
-    return (
-        <div>
+    const keyword = window.location.search.split('=')[1]
+    const [articlesList, setArticlesList] = useState([])
 
-        </div>
+    useEffect(() => {
+        console.log(keyword)
+        getArticlesList(keyword)
+        .then(articles => setArticlesList(articles.body.items))
+    }, [keyword])
+
+    return (
+        <section>
+            {
+                articlesList?.length > 0  ? 
+                    <div>
+                        {
+                            articlesList.map((item) => 
+                                <Items key={item.id} {...item} />
+                            )
+                        }
+                    </div>
+                    :
+                    <div className="not-found-container">
+                        <h2> No se encontraron resultados, intenta de nuevo </h2>
+                        <Link className="button--small--blue" to="/">Volver</Link>
+                    </div>
+            }
+        </section>
     )
 }
 
