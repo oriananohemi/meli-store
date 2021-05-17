4bb7395e86197790ea01d92113ca9ab51d6aebae
Oriana <oriana.nohemi22@gmail.com>
2021-05-16 22:37:56 -0500

    agrega rutas e informacion del autor

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const router = require("./network/routes");
 const config = require("./config");
 const app = express();
 
--- server/config.js
+++ server/config.js
@@ -1,7 +1,11 @@
 const config = {
     port: process.env.PORT || 3200,
     host: process.env.HOST || "http://localhost",
-    apiUrl: 'https://api.mercadolibre.com/sites/MLA/',
+    apiUrl: 'https://api.mercadolibre.com/',
+    author: {
+      name: 'Oriana',
+      lastname: 'Reina'
+    }
   };
   
   module.exports = config;
