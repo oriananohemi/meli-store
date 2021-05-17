50ad4505a54fd065ce0541743abbe3d39cb41676
Oriana <oriana.nohemi22@gmail.com>
2021-05-17 15:33:24 -0500

    eliminar console.log

--- server/components/products/network.js
+++ server/components/products/network.js
@@ -7,12 +7,10 @@ const router = express.Router()
 router.get('/items', (req, res) => {
     controller.getArticlesByQuery(req.query.q)
     .then((list) => {
-        console.log(list)
         response.success(req, res, list)
     })
     .catch((e) => {
-        console.error(e)
-        response.error(req,res, 'No se encuentran coincidencias', 400, e)
+        response.error(req,res, 'No se encuentran coincidencias', 404, e)
     })
 })
 
@@ -22,8 +20,7 @@ router.get('/items/:id', (req, res) => {
         response.success(req, res, article)
     })
     .catch((e) => {
-        console.log(e)
-        response.error(req,res, 'Articulo no encontrado', 400, e)
+        response.error(req,res, 'Articulo no encontrado', 404, e)
     })
 })
 
