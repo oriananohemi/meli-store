c2b7681814c20eca58adff626f271c8f9396df76
Oriana <oriana.nohemi22@gmail.com>
2021-05-17 10:14:51 -0500

    estructura y estilos de la lista de items

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import shipping from '../assets/images/shipping.png';
 
-const Items = () => {
+const Items = ({title, picture, price, free_shipping, address, condition}) => {
     return (
-        <div>
-
-        </div>
+        <article className="center-container item">
+            <img className="item__image" src={picture} alt={title} />
+            <div>
+                <p className="item__amount">{price.amount} <span>{price.currency}</span> {free_shipping?<img src={shipping} alt="Envio Gratis" /> : ''}</p>
+                <h1 className="item__title">{title}</h1>
+                {condition === 'new'? <p>Nuevo</p> : <p>Usado</p> }
+            </div>
+            <p>{address}</p>
+        </article>
     )
 }
 
